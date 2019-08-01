637092f0a0a92ae19bc1dcc3da2627eee55a55f6
titusaj <titusaj92@gmail.com>
2019-08-01 10:46:36 -0400

    figureing out how to label the noise in the system and then push take this labeled buffer and figure out the SNR

---
+++ Matlab/lungSoundQualityCheck.m
@@ -0,0 +1,24 @@
+% Titus John
+% Lung Sound Quality System
+% Matlab Implemention that need to be converted to C++
+
+%This code is meant to measure the snr of the signal in realtime
+
+% Input
+% the audio buffer is input into this system currently for processing (this
+% buffer can be of varying lengths
+
+%Output
+% The SNR value and boolean value indicating if the the sound is to loud
+
+
+function [SNR, qualityValue] = lungSoundQualityCheck(audioBuffer, Fs) 
+
+    
+
+
+
+
+
+
+end
